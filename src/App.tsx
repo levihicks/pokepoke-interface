31d1c7f8518ef132ffa78d212b44c6ca5e3eb2f5
levi hicks <lv.hicks@gmail.com>
2021-12-05 00:03:41 -0600

    add error dialog when user is on unsupported chain

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 import Container from '@mui/material/Container';
-import { CssBaseline } from '@mui/material';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import CssBaseline from '@mui/material/CssBaseline';
 import Header from './components/header';
 import Pokebar from './components/pokebar';
 import PokeList from './components/poke-list';
@@ -11,6 +14,23 @@ import { store } from './store';
 import { Provider } from 'react-redux';
 
 function App() {
+  const reloadPage = () => window.location.reload();
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  useEffect(() => {
+    (window as any).ethereum.on('chainChanged', reloadPage);
+    (window as any).ethereum
+      .request({ method: 'eth_chainId' })
+      .then((res: string) => {
+        if (['0x539'].indexOf(res) === -1) {
+          setDialogOpen(true);
+        }
+      });
+    return () => {
+      (window as any).ethereum.removeListener('chainChanged', reloadPage);
+    };
+  }, []);
+
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
@@ -19,6 +39,10 @@ function App() {
           <Header />
           <Pokebar />
           <PokeList />
+          <Dialog open={dialogOpen}>
+            <DialogTitle>Error: Chain not supported.</DialogTitle>
+            <DialogContent>Please switch to a supported chain.</DialogContent>
+          </Dialog>
         </Container>
       </ThemeProvider>
     </Provider>
